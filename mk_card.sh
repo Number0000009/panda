a9eeb26c1bea5ab6557aeb7306845e045edf7e61
motherf <no.0000000000009@gmail.com>
2018-11-11 12:26:21 +0000

    Since version 2.26 sfdisk does not provide a bunch of options

--- mk_card.sh
+++ mk_card.sh
@@ -1,17 +1,48 @@
 #!/bin/sh
+
+# exit if any command fails
+set -e
+
 if [ ! "$1" = "/dev/sda" ] ; then
-    DRIVE=$1
-    if [ -b "$DRIVE" ] ; then
-	dd if=/dev/zero of=$DRIVE bs=1024 count=1024
-	SIZE=`fdisk -l $DRIVE | grep Disk | awk '{print $5}'`
-	echo DISK SIZE - $SIZE bytes
-	CYLINDERS=`echo $SIZE/255/63/512 | bc`
-	echo CYLINDERS - $CYLINDERS
-	{
-	echo ,9,0x0C,*
-	echo ,,,-
-	} | sfdisk -D -H 255 -S 63 -C $CYLINDERS $DRIVE
-	mkfs.vfat -F 32 -n "boot" ${DRIVE}1
-	mke2fs -j -L "rootfs" ${DRIVE}2
-    fi 
+
+    CARD_DEV=$1
+
+    if [ ! -b "$CARD_DEV" ] ; then
+        echo $CARD_DEV is not a block device
+        exit 1
+    fi
+
+    if [ -b "$CARD_DEV" ] ; then
+        dd if=/dev/zero of=$CARD_DEV bs=1024 count=1024
+        SIZE=`fdisk -l $CARD_DEV | grep Disk | awk '{print $5}'`
+
+        echo DISK SIZE - $SIZE bytes
+
+        CYLINDERS=`echo $SIZE/255/63/512 | bc`
+
+        echo CYLINDERS - $CYLINDERS
+
+        {
+            echo 63,$SIZE,0x0C,*,,,-
+        } | sfdisk $CARD_DEV
+
+       mkfs.vfat -F 32 -n "BOOT" ${CARD_DEV}1
+# No ext2 rootfs
+#       mke2fs -j -L "rootfs" ${CARD_DEV}2
+    fi
+
+# Copy-pasted from google
+#    # so hopefully this will abort e.g. if we're about to blow away the root filesystem
+#    for mounted in $(findmnt -o source | grep "^$CARD_DEV") ; do
+#        umount "$mounted"
+#    done
+
+#    echo "*************** Formatting SD card... ***************"
+#    wipefs --all $CARD_DEV
+#    {
+#        echo 63,144585,0x0C,* echo 144585,,,-
+#    } | sfdisk $CARD_DEV
+#
+#    mkfs.vfat -F 32 -n "boot" ${CARD_DEV}1
+#    mke2fs -j -L "filesystem" ${CARD_DEV}2
 fi
